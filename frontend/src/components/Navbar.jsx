c2c92e46f62b99f6dcdc75c14f4268675ce5640e
Tearista <veronicalimjaya@gmail.com>
2026-01-23 18:39:58 +0700

    23/01/2026 - Make responsive for desktop, tablet and phone

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,48 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 // Import css
 import '../styling/navbar.css';
 
 function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const closeMenu = () => setMenuOpen(false);
     return (
         <>
-            {/* ================= NAVBAR ================= */}
             <div className='navbar-header'>
                 <div className='navbar-logo'>
                     <h1>Tearista Fruit</h1>
                 </div>
 
-                <div className='navbar-menus'>
+                <div className='menu-toggle' onClick={() => setMenuOpen(!menuOpen)}>
+                    <i className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+                </div>
+
+                <div className={`navbar-menus ${menuOpen ? 'active': ''}`}>
                     <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/product'>Product</Link></li>
-                        <li><Link to='/blog'>Blog</Link></li>
-                        <li><Link to='/contact'>Contact</Link></li>
+                        <li><NavLink to='/' onClick={closeMenu}>Home</NavLink></li>
+                        <li><NavLink to='/product' onClick={closeMenu}>Product</NavLink></li>
+                        <li><NavLink to='/blog' onClick={closeMenu}>Blog</NavLink></li>
+                        <li><NavLink to='/contact' onClick={closeMenu}>Contact</NavLink></li>
                     </ul>
+
+                    <div className='navbar-button-mobile'>
+                        <button className='helper-button'>
+                            <i className='fas fa-headset'></i>
+                            <span>Helper</span>
+                        </button>
+
+                        <button className='cart-button'>
+                            <i className='fas fa-shopping-cart'></i>
+                            <span>Cart</span>
+                        </button>
+
+                        <button className='login-button'>
+                            <i className='fas fa-user-circle'></i>
+                            <span>Login</span>
+                        </button>
+                    </div>
                 </div>
 
                 <div className='navbar-button'>
