500e2e9ca753439c588bec747c38aa2b04a5a078
Tearista <veronicalimjaya@gmail.com>
2026-02-07 16:41:26 +0700

    07/02/2026 - Add Order Endpoint, Integrate to Email

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
+// ENDPOINT
 app.get('/api/products', async (req, res) => {
     const { data, error } = await supabase
         .from('products')
@@ -44,7 +45,6 @@ app.get('/api/blogs', async (req, res) => {
 
 app.get('/api/blogs/:id', async (req, res) => {
     const { id } = req.params;
-
     const { data, error } = await supabase
         .from('blogs')
         .select('*')
@@ -54,6 +54,39 @@ app.get('/api/blogs/:id', async (req, res) => {
     if (error) return res.status(500).json({ error: error.message });
     if (!data) return res.status(404).json({ error: 'Blog not found' });
     
+    res.json(data);
+});
+
+app.post('/api/orders', async (req, res) => {
+    const { email, items, total_price, address, payment_method, shipping_fee } = req.body;
+
+    const { data, error } = await supabase
+        .from('orders')
+        .insert([
+            {
+                user_email: email,
+                items: items,
+                total_price: total_price,
+                address: address,
+                payment_method: payment_method,
+                shipping_fee: shipping_fee || 0,
+                status: 'Proses'
+            }
+        ]);
+
+    if (error) return res.status(500).json({ error: error.message });
+    res.json({ message: 'Order success', data });
+});
+
+app.get('/api/orders/:email', async (req, res) => {
+    const { email } = req.params;
+    const { data, error } = await supabase
+        .from('orders')
+        .select('*')
+        .eq('user_email', email)
+        .order('created_at', { ascending: false });
+
+    if (error) return res.status(500).json({ error: error.message });
     res.json(data);
 });
 
